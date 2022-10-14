b820d838ab99fc2826082f78c7c649971d548e57
AntoCast18 <101275905+AntoCast18@users.noreply.github.com>
2022-10-14 18:54:52 -0300

    implementacion para persona

--- backend/proyecto/src/main/java/com/proyecto/proyecto/Controller/PersonaController.java
+++ backend/proyecto/src/main/java/com/proyecto/proyecto/Controller/PersonaController.java
@@ -6,6 +6,7 @@ import com.proyecto.proyecto.Entity.Persona;
 import com.proyecto.proyecto.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Antonella
  */
 @RestController
+@CrossOrigin (origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
@@ -55,5 +57,10 @@ public class PersonaController {
 
         ipersonaService.savePersona(persona);
         return persona;
+    }
+    
+     @GetMapping("personas/traer/perfil")
+    public Persona findPersona(){
+       return ipersonaService.findPersona((long)1);
     }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { EducacionComponent } from './components/educacion/educacion.component';
 import { HysComponent } from './components/hys/hys.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { FooterComponent } from './components/footer/footer.component';
     BrowserModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
